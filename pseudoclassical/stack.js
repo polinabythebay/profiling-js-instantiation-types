ec94b33a9657d2f02e4fbc7224eb4b7d09e18d7e
Polina Soshnin <psoshnin@wellesley.edu>
2015-11-05 11:52:35 -0800

    add psuedoclassical instantiation type

---
+++ pseudoclassical/stack.js
@@ -0,0 +1,23 @@
+var Stack = function() {
+  this.storage = {};
+  this.count = 0;
+};
+
+Stack.prototype.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+Stack.prototype.pop = function(value) {
+  if (this.count === 0) {
+    return null;
+  }
+  var pop = this.storage[this.count-1];
+  delete this.storage[this.count];
+  this.count--;
+  return pop;
+};
+
+Stack.prototype.size = function() {
+  return this.count;
+};
